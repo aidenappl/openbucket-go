7a8245f55e9d89a9d247ae1daadc00c06644af5b
Aiden Appleby <aiden.appleby3@gmail.com>
2025-08-17 18:28:55 -0400

    cleaned up upload handler

--- routers/HandleUpload.router.go
+++ routers/HandleUpload.router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,40 +14,15 @@ import (
 	"github.com/aidenappl/openbucket-go/responder"
 	"github.com/aidenappl/openbucket-go/tools"
 	"github.com/aidenappl/openbucket-go/types"
+	"github.com/aidenappl/openbucket-go/util"
 	"github.com/gorilla/mux"
 )
 
 func HandleUpload(w http.ResponseWriter, r *http.Request) {
 
-	vars := mux.Vars(r)
-	bucket := vars["bucket"]
-	key := vars["key"]
-
-	if bucket == "" || key == "" {
-		http.Error(w, "Bucket and key must be provided", http.StatusBadRequest)
-		log.Println("Bucket or key is empty")
-		return
-	}
-
-	user := middleware.RetrieveSession(r)
-	if user == nil {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		log.Println("Unauthorized access attempt")
-		return
-	}
-
-	filePath := filepath.Join("buckets", bucket, key)
-
-	bucketDir := filepath.Join("buckets", bucket)
-	if _, err := os.Stat(bucketDir); os.IsNotExist(err) {
-		http.Error(w, "Bucket not found", http.StatusNotFound)
-		log.Println("Bucket not found:", bucketDir)
-		return
-	} else if err != nil {
-		http.Error(w, "Unable to access bucket", http.StatusInternalServerError)
-		log.Println("Error accessing bucket:", err)
-		return
-	}
+	// Get requestID and hostID
+	requestId := middleware.GetRequestID(r)
+	hostId := middleware.GetHostID(r)
 
 	// Check different subrequest types
 	found, _, _ := tools.HeaderExists(r, "x-amz-copy-source")
@@ -57,41 +33,87 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	if _, ok := q["partNumber"]; ok {
 		log.Println("Currently do not support multipart uploads")
-		http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		responder.SendXMLError(w, http.StatusNotImplemented, "NotImplemented", "Multipart uploads are not supported", requestId, hostId)
 		return
 	}
 	if _, ok := q["uploadId"]; ok {
 		log.Println("Currently do not support multipart uploads")
-		http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		responder.SendXMLError(w, http.StatusNotImplemented, "NotImplemented", "Multipart uploads are not supported", requestId, hostId)
 		return
 	}
 	if _, ok := q["acl"]; ok {
 		log.Println("Currently do not support acl")
-		http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		responder.SendXMLError(w, http.StatusNotImplemented, "NotImplemented", "ACL is not supported", requestId, hostId)
 		return
 	}
 	if _, ok := q["tagging"]; ok {
 		log.Println("Currently do not support tagging")
-		http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		responder.SendXMLError(w, http.StatusNotImplemented, "NotImplemented", "Tagging is not supported", requestId, hostId)
 		return
 	}
 	if _, ok := q["legal-hold"]; ok {
 		log.Println("LegalHold query parameter is not supported for bucket operations")
-		responder.SendXMLError(w, http.StatusBadRequest, "InvalidRequest", "LegalHold query parameter is not supported", "", "")
+		responder.SendXMLError(w, http.StatusBadRequest, "InvalidRequest", "LegalHold query parameter is not supported", requestId, hostId)
 		return
 	}
 	if _, ok := q["retention"]; ok {
 		log.Println("Retention query parameter is not supported for bucket operations")
-		responder.SendXMLError(w, http.StatusBadRequest, "InvalidRequest", "Retention query parameter is not supported", "", "")
+		responder.SendXMLError(w, http.StatusBadRequest, "InvalidRequest", "Retention query parameter is not supported", requestId, hostId)
 		return
 	}
 
+	handleUpload(w, r)
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	// Get request variables
+	vars := mux.Vars(r)
+	bucket := vars["bucket"]
+	rawKey := vars["key"]
+
+	// Get hostID and requestID
+	hostId := middleware.GetHostID(r)
+	requestId := middleware.GetRequestID(r)
+
+	// Decode key if URL-encoded (supports spaces, etc.)
+	key, err := url.PathUnescape(rawKey)
+	if err != nil {
+		responder.SendXMLError(w, http.StatusBadRequest, "InvalidKey", "Failed to decode key", requestId, hostId)
+		log.Println("Error decoding key:", err)
+		return
+	}
+
+	// Validate bucket and key
+	if bucket == "" || key == "" {
+		responder.SendXMLError(w, http.StatusBadRequest, "InvalidBucketOrKey", "Bucket and key must be provided", requestId, hostId)
+		log.Println("Bucket or key is empty")
+		return
+	}
+
+	// Gather the user session
+	user := middleware.RetrieveSession(r)
+	if user == nil {
+		responder.SendAccessDeniedXML(w, &requestId, &hostId)
+		log.Println("Unauthorized access attempt")
+		return
+	}
+
+	// Setup the filepath
+	filePath := filepath.Join("buckets", bucket, key)
+
+	// Validate bucket exists
+	if !util.BucketExists(bucket) {
+		responder.SendXMLError(w, http.StatusNotFound, "NoSuchBucket", "the requested bucket does not exist", requestId, hostId)
+		log.Println("requested bucket does not exist")
+		return
+	}
+
+	// Check if the bucket is a directory
 	isDirectory := strings.HasSuffix(key, "/")
 	if isDirectory {
-
 		err := os.MkdirAll(filePath, os.ModePerm)
 		if err != nil {
-			http.Error(w, "Failed to create directory", http.StatusInternalServerError)
+			responder.SendXMLError(w, http.StatusInternalServerError, "InternalError", "Failed to create directory", requestId, hostId)
 			log.Println("Error creating directory:", err)
 			return
 		}
