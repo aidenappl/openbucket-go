b02bea88d98af4671f1746f4ce2f444a2663f4f8
Aiden Appleby <aiden.appleby3@gmail.com>
2026-01-24 17:18:18 -0500

    removing check for accept-encoding

--- aws/ValidateSignature.aws.go
+++ aws/ValidateSignature.aws.go
@@ -120,11 +120,17 @@ func buildCanonicalRequest(r *http.Request,
 		r.Header.Set("Host", r.Host)
 	}
 
+	// Headers that proxies commonly modify - exclude from signature validation
+	// These are unreliable for signature verification in proxied environments
+	proxyModifiedHeaders := map[string]bool{
+		"accept-encoding": true,
+	}
+
 	hdrNames := strings.Split(signedHeadersCSV, ";")
 	var clean []string
 	for _, h := range hdrNames {
 		h = strings.TrimSpace(h)
-		if h != "" {
+		if h != "" && !proxyModifiedHeaders[strings.ToLower(h)] {
 			clean = append(clean, h)
 		}
 	}
